323beb4c562002e5c490efb461948f0d9a3c8039
cemerdem <cem.erdem@servian.com>
2022-04-17 15:02:54 +1000

    seasons project files

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
+import {findAllByDisplayValue} from "@testing-library/react";
 
-class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {lat: null};
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {lat: null, errorMessage: ''};
+    //     // don't do data loading here (not recommended). Instead, do it in componentDidMount
+    // }
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position)=> {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => console.log(err)
-        );
+            (
+                position) => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message}));
     }
-    // we have to define render
-    render(){
-        return <div> Latitude: {this.state.lat}</div>;
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <Spinner message='Please accept location request'/>;
+    }
+
+    // we have to define render and is only used to return JSX
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
